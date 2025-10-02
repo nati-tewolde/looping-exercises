7ad5ec8d924de51635c9a1ba4106aecaa577ec19
nati-tewolde <natisnewemail@gmail.com>
2025-10-02 11:37:05 -0400

    refactored ForLoop solution

--- src/main/java/com/pluralsight/ForLoop.java
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -3,14 +3,21 @@ package com.pluralsight;
 public class ForLoop {
     public static void main(String[] args) throws InterruptedException {
 
-        for (int i = 10; i >= 0; i--) {
+        /*for (int i = 10; i >= 0; i--) {
             if (i == 0) {
                 System.out.println("Launch!");
                 break;
             }
             System.out.println(i);
             Thread.sleep(1000);
+        }*/
+
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+            Thread.sleep(1000);
         }
+
+        System.out.println("Launch!");
 
     }
 }
